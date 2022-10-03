89cec48d341401e74f62fa00a1938c5a408b989b
sooyoungh <sooyoung.h8@gmail.com>
2022-10-04 00:35:35 +0900

    컨트롤러/User 도메인 추가

---
+++ src/main/java/com/example/security/controller/IndexController.java
@@ -0,0 +1,66 @@
+package com.example.security.controller;
+
+import com.example.security.domain.User;
+import com.example.security.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class IndexController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @GetMapping({ "", "/" })
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/user")
+    public @ResponseBody String user() {
+        return "유저 페이지입니다.";
+    }
+
+    @GetMapping("/admin")
+    public @ResponseBody String admin() {
+        return "어드민 페이지입니다.";
+    }
+
+
+    @GetMapping("/loginForm")
+    public String loginForm() {
+        return "loginForm";
+    }
+
+    // 회원가입할 수 있는 Form 페이지
+    @GetMapping("/joinForm")
+    public String joinForm() {
+        return "joinForm";
+    }
+
+    // 회원가입
+    @GetMapping("/join")
+    public @ResponseBody String join() {
+        return "join";
+    }
+
+
+    // 회원가입 From 의 액션
+    @PostMapping("/joinProc")
+    public String joinProc(User user) {
+        System.out.println("회원가입 진행 : " + user);
+        String rawPassword = user.getPassword();
+        String encPassword = bCryptPasswordEncoder.encode(rawPassword); // 암호화
+        user.setPassword(encPassword);
+        user.setRole("ROLE_USER");
+        userRepository.save(user);
+        return "redirect:/loginForm";
+    }
+}
